c9967d5755ce106c56a6eeccf98772cc0ebcd062
jswalsakshi <jswal.sakshi@gmail.com>
2020-07-24 17:20:27 +0530

    updated code

--- FindIn/Controller/ViewController.swift
+++ FindIn/Controller/ViewController.swift
@@ -12,23 +12,51 @@ import AloeStackView
 class ViewController: AloeStackViewController {
     
     let mainView = AloeStackView()
+    
+    var profileView: UIImageView = {
+        let profileView = UIImageView()
+        profileView.adjustsImageSizeForAccessibilityContentSizeCategory = true
+        let image = UIImage(named: "placeholder")
+        profileView.layer.borderWidth = 1.0
+        profileView.layer.masksToBounds = false
+        profileView.layer.borderColor = UIColor.white.cgColor
+        profileView.image = image
+        profileView.clipsToBounds = true
+        profileView.layer.cornerRadius = (image?.size.width)!/2
+        return profileView
+    }()
+    
+    var nameLabel: UILabel = {
+        let nameLabel = UILabel()
+        nameLabel.text = "Name"
+        nameLabel.textColor = UIColor.gray
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        return nameLabel
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setUpNavigationBarView()
-        //view.backgroundColor = .black
+        self.setUpProfileView()
+        self.setUpNameLabel()
     }
 
 
     func setUpNavigationBarView() {
         navigationController?.navigationBar.barTintColor = .black
-        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.isTranslucent = true
         navigationItem.title = "User Detail"
         navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
     }
     
-    func setUpView() {
-        
+    func setUpProfileView() {
+//        stackView.setInset(forRows: stackView.getAllRows(), inset: UIEdgeInsets(top: 0, left: 24, bottom: 0, right: -24))
+        stackView.addRow(profileView)
+        profileView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+    }
+    
+    func setUpNameLabel() {
+        stackView.addRow(nameLabel)
     }
 }
 
